d9115bee93b1051a395bb9c77674f83609f8ecff
Jon Nordby <jononor@gmail.com>
2011-08-28 13:47:37 +0200

    ArchLinux: Can use PackageKit to resolve dependencies

--- deploymentkit/archlinux.py
+++ deploymentkit/archlinux.py
@@ -79,13 +79,80 @@ def package_from_executable(executable_name):
     """Return the name of the package that should be used to provide @executable_name"""
     raise NotImplementedError
 
+
 def package_from_pkgconfig(pkg_config_string):
     """Return the name of package that should be used to provide @pkg_config_strig."""
 
-    # FIXME: this is mostly not Arch specific, put somewhere shared
-    # TODO: Use PackageKit as the primary way of querying this
     # TODO: handle version requirements
     pkg_config_id = pkg_config_string
+
+    # TODO: introduce a (target platform) configuration,
+    # and define these variables there
+    use_packagekit = False
+    if use_packagekit:
+        return package_from_pkgconfig_packagekit(pkg_config_id)
+    else:
+        return package_from_pkgconfig_fallback(pkg_config_id)
+
+def package_from_pkgconfig_packagekit(pkg_config_id):
+
+    # FIXME: use packagekit-python, instead of pkcon
+    # As of 28.08 on Arch Linux the search_file query fails
+    # with unable to find dbus method SearchFiles with signature (ss)
+
+    #import packagekit.client
+    #pk_client = packagekit.client.PackageKitClient()
+    #packages = pk_client.search_file(pkg_config_id + '.pc')
+
+    pkg_config_file = pkg_config_id + '.pc'
+    output = subprocess.check_output(['pkcon', 'search', 'file', pkg_config_file])
+    # Output is on the form
+    #Transaction:	Searching by name
+    #Status: 	Querying
+    #Package:	gegl-0.1.6-1.x86_64
+    #Package:	gegl-gtk-git-20110721-1.x86_64
+    #Results:
+    #Installed    gegl-0.1.6-1.x86_64
+    #Installed    gegl-gtk-git-20110721-1.x86_64
+
+    packages = []
+    installed_packages = []
+    for line in output.split("\n"):
+        if line.startswith('Package:'):
+            pkg_id = line.split('Package:')[1].strip()
+            packages.append(pkg_id)
+        if line.startswith('Installed'):
+            pkg_id = line.split('Installed')[1].strip()
+            installed_packages.append(pkg_id)
+
+    if len(packages) == 0:
+        return ''
+    elif len(packages) == 1:
+        pkg_id = packages[0]
+    else:
+        # XXX: Can we handle mutiple packages providing the same file better?
+        # FIXME: handle architecture differences correctly
+        # TODO: use logging module instead
+        print "Warning: Multiple package matches found for pkgconfig identifier %s" % pkg_config_id
+        pkg_id = packages[0]
+
+    # Strip away architecture
+    # gegl-gtk-git-20110721-1.x86_64 -> gegl-gtk-git-20110721-1
+    pkg = pkg_id[0:pkg_id.rindex('.')]
+
+    # Arch Linux specific
+    # Strip away version number
+    # Packages are on format:
+    # $pkgname-$version-$build_revision
+    # where only $pkgname can contain dashes
+    # gegl-gtk-git-20110721-1 -> gegl-gtk-git
+    pkg_name = '.'.join(pkg.split('-')[0:-2])
+
+    return pkg_name
+
+def package_from_pkgconfig_fallback(pkg_config_id):
+
+    # FIXME: this is mostly not Arch specific, put somewhere shared
 
     # Check if package is installed
     args = ['pkg-config', '--exists', pkg_config_id]
@@ -96,7 +163,9 @@ def package_from_pkgconfig(pkg_config_string):
         raise NotImplementedError
 
     # From man pkg-config
+    # FIXME: handle lib64 correctly
     # TODO: find a more reliable way to determine the directories
+    # * Get a --filename option into pkg-config, perhaps?
     # TODO: handle PKG_CONFIG_LIBDIR and PKG_CONFIG_PATH
     pkg_config_dirs = ['/usr/lib/pkgconfig',
             '/usr/share/pkgconfig',
@@ -136,7 +205,7 @@ def create_pkgbuild(metadata):
 def pkgbuild_attribute(pkgbuild, attribute):
     """Return the value of a given @attribute in @pkgbuild.
     The entire value string is returned, or empty string if the attribute is not found.
-    NB: Not anywhere close to a decent PKGBUILD parser, only designed to work on 
+    NB: Not anywhere close to a decent PKGBUILD parser, only designed to work on
     well-known, super-well-formed input. Used for testing out PKGBUILD generator.
     """
 
